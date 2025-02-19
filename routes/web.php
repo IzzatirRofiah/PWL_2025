8918b048c2aa209fa677989ab9a7bd59775d9986
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-02-19 16:18:46 +0700

    Modif web

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-
-// Route::get('/hello', function () {
-//         return 'Hello World';
-
-// Route::get('/world', function () {
-//         return 'World';   
-
-// Route::get('/', function () {
-//         return 'Selamat Datang';     
-
-Route::get('/about', function () {
-        return "NIM : 2341760010 <br> Nama : Izzatir Rofiah";
+Route::get('/', function () {
+    return view('welcome');
 });
+
+// PRAKTIKUM 1
+// Modif P1.1
+Route::get('/hello', function () {
+        return 'Hello World';
+});
+
+// Modif P1.2
+Route::get('/world', function () {
+        return 'World';  
+}); 
+
+// Modif P1.3
+Route::get('/', function () {
+        return 'Selamat Datang';  
+});   
+
+// Modif P1.4
+Route::get('/about', function () {
+        return 'NIM : 2341760010 <br> Nama : Izzatir Rofiah';
+});
+
+// PRAKTIKUM 2
+// Modif P2.1
+Route::get('/user/{name}', function ($name) {
+        return 'Nama saya ' . $name;
+});
+
+// Modif P2.2
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+// Modif P2.3
+Route::get('/articles/{id}', function ($id) {
+        return "Halaman Artikel dengan ID " . $id;
+});
+
+// PRAKTIKUM 3
+// Modif P3.1
+Route::get('/user/{name?}', function ($name=null) {
+        return 'Nama saya '.$name;
+});
+
+// Modif P3.2
+Route::get('/user/{name?}', function ($name='John') {
+        return 'Nama saya '.$name;
+});      
