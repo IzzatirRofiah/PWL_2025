d3b428547b6845bda73754b8b27fd34869328192
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-02-19 15:38:26 +0700

    modif3&4

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/hello', function () {
 //         return 'Hello World';
 
-Route::get('/world', function () {
-        return 'World';       
+// Route::get('/world', function () {
+//         return 'World';   
+
+// Route::get('/', function () {
+//         return 'Selamat Datang';     
+
+Route::get('/about', function () {
+        return "NIM : 2341760010 <br> Nama : Izzatir Rofiah";
 });
